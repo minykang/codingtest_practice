1c147903f400d1263c9d985bf60856e65c2f698a
강민영 <algh5304@naver.com>
2025-12-26 11:14:28 +0900

    [Silver IV] Title: 30, Time: 164 ms, Memory: 39276 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,22 @@
+#30으로 나누어 떨어지는 수 만들기
+"""
+보면 숫자에서 요소별로 빼와서 그걸 왔다갔다해서 바꾸는건데
+그리고 30의 배수이면 끝이 무조건 0이 되어야하는데 그럼 숫자중에 0 없으면 
+-1 출력하게 하면되는거고옮기기 위해서는 각각을 어케해야 옮길수 있지?
+"""
+n = input()
+ans = 0
+if "0" not in n:
+    print(-1)
+else:
+    if int(n) % 30 == 0:
+        print(int(n))
+    else:
+        for i in range(len(n)):
+            ans += int(n[i])
+        if ans % 3 == 0:
+            arr = list(n)
+            arr.sort(reverse = True)
+            print(''.join(arr))
+        else:
+            print(-1)

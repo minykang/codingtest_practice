72b187e1bfea1cb950a4847228f02da1fdb2e775
강민영 <algh5304@naver.com>
2026-02-10 12:36:01 +0900

    [Silver IV] Title: 스택, Time: 400 ms, Memory: 112044 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+stack = []
+
+for i in range(n):
+    order = input().split()
+    if order[0] == "push" :
+        stack.append(order[1])
+    if order[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    if order[0] == "size":
+        print(len(stack))
+    if order[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+    if order[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+        
+    
